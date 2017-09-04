f5008f3040c80437eedf486e641d112683cb5ee7
sasonhe <642139009qq.com>
2017-09-05 01:56:51 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
-
+    <div>
+        <hello></hello>
+    </div>
 </template>
 <script>
+import hello from './components/hello.vue'
 export default{
     components:{
-
+        hello
     }
 }
 </script>
--- src/components/hello.vue
+++ src/components/hello.vue
@@ -1,5 +1,7 @@
 <template>
-
+    <div>
+        <h1>hello Vue</h1>
+    </div>
 </template>
 <script>
 export default{
@@ -9,5 +11,5 @@ export default{
 }
 </script>
 <style scoped>
-
+div{width: 100px;height: 100px;margin: 50px auto;text-align: center;}
 </style>
