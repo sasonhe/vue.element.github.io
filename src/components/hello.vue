f604d6120fcc67ab0a79751b455dbeff65585a3d
sasonhe <642139009qq.com>
2017-09-05 03:34:27 +0800

    file

--- src/components/hello.vue
+++ src/components/hello.vue
@@ -1,15 +1,35 @@
-<template>
+<template lang="html">
     <div>
-        <h1>hello Vue</h1>
+        <el-menu theme="dark" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+          <el-menu-item index="1">处理中心</el-menu-item>
+          <el-submenu index="2">
+            <template slot="title">我的工作台</template>
+            <el-menu-item index="2-1">选项1</el-menu-item>
+            <el-menu-item index="2-2">选项2</el-menu-item>
+            <el-menu-item index="2-3">选项3</el-menu-item>
+          </el-submenu>
+          <el-menu-item index="3"><a >订单管理</a></el-menu-item>
+        </el-menu>
+        <!-- <div class="line"></div> -->
+
     </div>
 </template>
-<script>
-export default{
-    components:{
 
-    }
-}
-</script>
-<style scoped>
-div{width: 100px;height: 100px;margin: 50px auto;text-align: center;}
+<style lang="css">
+
 </style>
+<script>
+  export default {
+    data() {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1'
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
 import App from './App.vue'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
 // 引入路由配置
 import routes from './router.config'
-//全局样式重置
-import reset from './common/reset.css'
 // 注册路由
+Vue.use(Element)
 Vue.use(VueRouter)
 Vue.use(VueResource)
 
@@ -16,9 +17,9 @@ var router = new VueRouter({
 })
 
 new Vue({
-    el : '#app',
+    el: '#app',
     router,
-    render : function(h){
+    render: function(h) {
         return h(App)
     }
 })
