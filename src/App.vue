8192b0815a0272de362bf010fcb34a8f3ef99372
sasonhe <642139009@qq.com>
2017-09-07 14:23:15 +0800

    add admin

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,15 @@
     <div>
         <!-- test -->
         <hello></hello>
+        <admin></admin>
     </div>
 </template>
 <script>
 import hello from './components/hello.vue'
+import admin from './components/admin.vue'
 export default{
     components:{
-        hello
+        hello,admin
     }
 }
 </script>
