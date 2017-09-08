d51877cce5a1bb84e8bb2adda11c09df74a66469
sasonhe <642139009@qq.com>
2017-09-08 20:13:31 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
     <div>
         <!-- test -->
-        <hello></hello>
+        <!-- <hello></hello> -->
         <admin></admin>
     </div>
 </template>
 <script>
-import hello from './components/hello.vue'
+// import hello from './components/hello.vue'
 import admin from './components/admin.vue'
 export default{
     components:{
-        hello,admin
+        // hello,
+        admin
     }
 }
 </script>
